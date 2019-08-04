a8cb8e6fbedc691e188ed6ef26f99401e6fdafe3
haoch <haoch@DESKTOP-QKM1BR5>
2019-08-04 01:56:31 -0500

    TODO fix chp7 error

--- EmployeeManagementServerApplication/src/test/java/Test1.java
+++ EmployeeManagementServerApplication/src/test/java/Test1.java
@@ -1,10 +1,20 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.virtualpairprogrammers.employeemanagement.domain.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class Test1 {
 
@@ -25,8 +35,26 @@ public class Test1 {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testJpa() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("employeeDb");
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		//Employee employee1 = new Employee("James","Smith","Engineer",1000);
+		//em.persist(employee1);
+		
+		Query q = em.createQuery("select employee from Employee employee");
+		List<Employee> results = q.getResultList();
+		
+		for (Employee employee : results) {
+			System.out.println(employee);
+		}
+		
+		
+		tx.commit();
+		em.close();
 	}
 
 }
