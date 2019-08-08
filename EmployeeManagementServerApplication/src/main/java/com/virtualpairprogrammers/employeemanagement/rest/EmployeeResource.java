b8b9c6b63f49be61891812af9caa2ef75b4c6e52
htang07 <kim.tim20007@yahoo.com>
2019-08-08 16:53:02 -0500

    added EmployeeResource

---
+++ EmployeeManagementServerApplication/src/main/java/com/virtualpairprogrammers/employeemanagement/rest/EmployeeResource.java
@@ -0,0 +1,34 @@
+package com.virtualpairprogrammers.employeemanagement.rest;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.virtualpairprogrammers.employeemanagement.EmployeeManagementServiceLocal;
+import com.virtualpairprogrammers.employeemanagement.domain.Employee;
+
+@Stateless
+@Path("/employees")
+public class EmployeeResource {
+
+	@Inject
+	private EmployeeManagementServiceLocal service;
+	
+	@GET
+	@Produces("application/XML")
+	public List<Employee> getAllEmployees() {
+		return service.getAllEmployees();
+	}
+	
+	@GET
+	@Produces("application/XML")
+	@Path("{employeeNo}")
+	public Employee findEmployeeById(@PathParam("employeeNo") int id) {
+		return service.getById(id);
+	}
+}
