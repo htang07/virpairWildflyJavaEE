90087c38dab124ee423000e3e0437dde4245fb1e
haoch <haoch@DESKTOP-QKM1BR5>
2019-08-13 21:43:23 -0500

    added message producer

--- BookStorePOS/src/test/java/ProducerClientTest1.java
+++ BookStorePOS/src/test/java/ProducerClientTest1.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
@@ -38,9 +39,9 @@ public class ProducerClientTest1 {
 	}
 
 	@Test
-	public void testSend() {
+	public void testSendTextMessage() {
 		Properties jndiProperties = new Properties();
-		jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+		jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming"); //for wildfly
 		jndiProperties.put("jboss.naming.client.ejb.context","true");
 		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY,"org.jboss.naming.remote.client.InitialContextFactory");
 		jndiProperties.put(Context.PROVIDER_URL,"http-remoting://localhost:8080");
@@ -49,22 +50,58 @@ public class ProducerClientTest1 {
 		
 		try {
 			Context ctx = new InitialContext(jndiProperties);
-			Queue queue = (Queue)ctx.lookup("jms/BookStoreQueue");
+			Queue queue = (Queue)ctx.lookup("jms/BookStoreQueue"); //by convention name queue with the prefix jms/
 			ConnectionFactory cf = (ConnectionFactory)ctx.lookup("jms/RemoteConnectionFactory");
 			connection = cf.createConnection();
 			
 			Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
 			MessageProducer messageProducer = session.createProducer(queue);
 			
-			TextMessage message = session.createTextMessage("Hello World. The time is now " + new Date());
+			TextMessage message = session.createTextMessage("Received message. The time is now " + new Date());
 			
 			messageProducer.send(message);
 			
-		} catch (NamingException e) {
+		} catch (NamingException | JMSException e) {
 			e.printStackTrace();
-		} catch (JMSException e) {
-			e.printStackTrace();
+		} 
+		finally {
+			try {
+				connection.close();
+			} catch (JMSException e) {
+				
+			}
 		}
+	}
+	
+	@Test
+	public void testSendMapMessage() {
+		Properties jndiProperties = new Properties();
+		jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming"); //for wildfly
+		jndiProperties.put("jboss.naming.client.ejb.context","true");
+		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY,"org.jboss.naming.remote.client.InitialContextFactory");
+		jndiProperties.put(Context.PROVIDER_URL,"http-remoting://localhost:8080");
+		
+		Connection connection = null;
+		
+		try {
+			Context ctx = new InitialContext(jndiProperties);
+			Queue queue = (Queue)ctx.lookup("jms/BookStoreQueue"); //by convention name queue with the prefix jms/
+			ConnectionFactory cf = (ConnectionFactory)ctx.lookup("jms/RemoteConnectionFactory");
+			connection = cf.createConnection();
+			
+			Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+			MessageProducer messageProducer = session.createProducer(queue);
+			
+			MapMessage message = session.createMapMessage();
+			message.setInt("sku", 102);
+			message.setString("title", "Mastering Messaging");
+			message.setDouble("price", 10.99);
+			message.setLong("date", new Date().getTime());
+			messageProducer.send(message);
+			
+		} catch (NamingException | JMSException e) {
+			e.printStackTrace();
+		} 
 		finally {
 			try {
 				connection.close();
